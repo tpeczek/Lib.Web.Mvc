8687874a715610c3eb237f39703d5ef5b50082a7
Tomasz Pęczek <tpeczek@gmail.com>
2012-04-01 18:30:31 +0200

    Lib.Web.Mvc - Added AcceptAjaxRequestAttribute, AjaxRequestAttribute, NoAjaxRequestAttribute

---
+++ Lib.Web.Mvc/AcceptAjaxRequestAttribute.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Reflection;
+
+namespace Lib.Web.Mvc
+{
+    /// <summary>
+    /// Represents an attribute that specifies if action method will respond to AJAX request or not.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
+    public sealed class AcceptAjaxRequestAttribute : ActionMethodSelectorAttribute
+    {
+        #region Properties
+        /// <summary>
+        /// Gets value defining if action method will respond to AJAX request or not.
+        /// </summary>
+        public bool Accept { get; private set; }
+        #endregion
+
+        #region Constructor
+        /// <summary>
+        /// Initializes a new instance of the AcceptAjaxRequestAttribute class.
+        /// </summary>
+        /// <param name="accept">Defines if action method will respond to AJAX request or not.</param>
+        public AcceptAjaxRequestAttribute(bool accept)
+        {
+            Accept = accept;
+        }
+        #endregion
+
+        #region ActionMethodSelectorAttribute Members
+        /// <summary>
+        /// Determines whether a request is a valid request.
+        /// </summary>
+        /// <param name="controllerContext">The context within which the controller operates. The context information includes the controller, HTTP content, request context, and route data.</param>
+        /// <param name="methodInfo">Encapsulates information about a method, such as its type, return type, and arguments.</param>
+        /// <returns>True if the request is valid; otherwise, false.</returns>
+        public override bool IsValidForRequest(ControllerContext controllerContext, MethodInfo methodInfo)
+        {
+            if (controllerContext == null)
+                throw new ArgumentNullException("controllerContext");
+
+            bool isAjaxRequest = (controllerContext.HttpContext.Request["X-Requested-With"] == "XMLHttpRequest") || ((controllerContext.HttpContext.Request.Headers != null) && (controllerContext.HttpContext.Request.Headers["X-Requested-With"] == "XMLHttpRequest"));
+
+            return Accept == isAjaxRequest;
+        }
+        #endregion
+    }
+}
---
+++ Lib.Web.Mvc/AjaxRequestAttribute.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Reflection;
+
+namespace Lib.Web.Mvc
+{
+    /// <summary>
+    /// Represents an attribute that is used to restrict an action method so that the method handles only AJAX requests.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
+    public sealed class AjaxRequestAttribute : ActionMethodSelectorAttribute
+    {
+        #region Fields
+        private static readonly AcceptAjaxRequestAttribute _innerAttribute = new AcceptAjaxRequestAttribute(true);
+        #endregion
+
+        #region ActionMethodSelectorAttribute Members
+        /// <summary>
+        /// Determines whether a request is a valid AJAX request.
+        /// </summary>
+        /// <param name="controllerContext">The context within which the controller operates. The context information includes the controller, HTTP content, request context, and route data.</param>
+        /// <param name="methodInfo">Encapsulates information about a method, such as its type, return type, and arguments.</param>
+        /// <returns>True if the request is valid; otherwise, false.</returns>
+        public override bool IsValidForRequest(ControllerContext controllerContext, MethodInfo methodInfo)
+        {
+            return _innerAttribute.IsValidForRequest(controllerContext, methodInfo);
+        }
+        #endregion
+    }
+}
