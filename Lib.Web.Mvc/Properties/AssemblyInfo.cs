0eadc13fe796edeb032f5480295505e9b26fca20
Tomasz Pęczek <tpeczek@gmail.com>
2013-02-15 09:24:01 +0100

    [v6.2.0] Lib.Web.Mvc

--- Lib.Web.Mvc/Properties/AssemblyInfo.cs
+++ Lib.Web.Mvc/Properties/AssemblyInfo.cs
@@ -10,27 +10,13 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Tomasz Pęczek")]
 [assembly: AssemblyProduct("Lib.Web.Mvc")]
-[assembly: AssemblyCopyright("Copyright ©  2009 - 2012 Tomasz Pęczek")]
+[assembly: AssemblyCopyright("Copyright © 2009 - 2013 Tomasz Pęczek")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("68e6372a-38aa-427a-b5f8-1952175d5ccd")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("6.1.0.0")]
-[assembly: AssemblyFileVersion("6.1.0.0")]
+[assembly: AssemblyVersion("6.2.0.0")]
+[assembly: AssemblyFileVersion("6.2.0.0")]
