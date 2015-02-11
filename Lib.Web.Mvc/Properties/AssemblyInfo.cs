407f8416c3fc788c024a774e06292913d8d7644c
Tomasz Pęczek <tpeczek@gmail.com>
2015-02-11 21:12:24 +0100

    Update Copyright

--- Lib.Web.Mvc/Properties/AssemblyInfo.cs
+++ Lib.Web.Mvc/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Tomasz Pęczek")]
 [assembly: AssemblyProduct("Lib.Web.Mvc")]
-[assembly: AssemblyCopyright("Copyright © 2009 - 2014 Tomasz Pęczek")]
+[assembly: AssemblyCopyright("Copyright © 2009 - 2015 Tomasz Pęczek")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
