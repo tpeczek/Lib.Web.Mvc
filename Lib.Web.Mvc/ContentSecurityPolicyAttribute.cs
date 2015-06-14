fa3422b577bbf83541d098e04f467cb67268ead9
Tomasz Pęczek <tpeczek@gmail.com>
2015-06-14 22:32:22 +0200

    Added basic Content Security Policy support
    Added ContentSecurityPolicyAttribute and ContentSecurityPolicyExtensions
    with support for script-src directive

---
+++ Lib.Web.Mvc/ContentSecurityPolicyAttribute.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+
+namespace Lib.Web.Mvc
+{
+    /// <summary>
+    /// Content Security Policy Level 2 inline execution modes
+    /// </summary>
+    public enum ContentSecurityPolicyInlineExecution
+    {
+        /// <summary>
+        /// Refuse any inline execution
+        /// </summary>
+        Refuse,
+        /// <summary>
+        /// Allow all inline execution
+        /// </summary>
+        Unsafe,
+        /// <summary>
+        /// Use nonce mechanism
+        /// </summary>
+        Nonce,
+        /// <summary>
+        /// Use hash mechanism
+        /// </summary>
+        Hash
+    }
+
+    /// <summary>
+    /// Action filter for defining Content Security Policy Level 2 policies
+    /// </summary>
+    public sealed class ContentSecurityPolicyAttribute : FilterAttribute, IActionFilter, IResultFilter
+    {
+        #region Constants
+        internal const string ScriptInlineExecutionContextKey = "Lib.Web.Mvc.ContentSecurityPolicy.ScriptInlineExecution";
+        internal const string NonceRandomContextKey = "Lib.Web.Mvc.ContentSecurityPolicy.NonceRandom";
+        internal const string ScriptHashListBuilderContextKey = "Lib.Web.Mvc.ContentSecurityPolicy.ScriptHashListBuilder";
+
+        private const string _scriptHashListPlaceholder = "<ScriptHashListPlaceholder>";
+
+        private const string _contentSecurityPolicyHeader = "Content-Security-Policy";
+        private const string _directivesDelimiter = ";";
+        private const string _scriptDirective = "script-src";
+        private const string _unsafeInlineSource = " 'unsafe-inline'";
+        private const string _nonceSourceFormat = " 'nonce-{0}'";
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// Gets or sets the source list for script-src directive.
+        /// </summary>
+        public string ScriptSource { get; set; }
+
+        /// <summary>
+        /// Gets or sets the inline execution mode for scripts
+        /// </summary>
+        public ContentSecurityPolicyInlineExecution ScriptInlineExecution { get; set; }
+        #endregion
+
+        #region Constructor
+        /// <summary>
+        /// Initializes new instance of ContentSecurityPolicyAttribute
+        /// </summary>
+        public ContentSecurityPolicyAttribute()
+        {
+            ScriptInlineExecution = ContentSecurityPolicyInlineExecution.Refuse;
+        }
+        #endregion
+
+        #region IActionFilter Members
+        /// <summary>
+        /// Called after the action method executes.
+        /// </summary>
+        /// <param name="filterContext"></param>
+        public void OnActionExecuted(ActionExecutedContext filterContext)
+        { }
+
+        /// <summary>
+        /// Called before an action method executes.
+        /// </summary>
+        /// <param name="filterContext"></param>
+        public void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            StringBuilder policyBuilder = new StringBuilder();
+
+            if (!String.IsNullOrWhiteSpace(ScriptSource) || (ScriptInlineExecution != ContentSecurityPolicyInlineExecution.Refuse))
+            {
+                policyBuilder.Append(_scriptDirective);
+
+                if (!String.IsNullOrWhiteSpace(ScriptSource))
+                {
+                    policyBuilder.AppendFormat(" {0}", ScriptSource);
+                }
+
+                filterContext.HttpContext.Items[ScriptInlineExecutionContextKey] = ScriptInlineExecution;
+                switch (ScriptInlineExecution)
+                {
+                    case ContentSecurityPolicyInlineExecution.Unsafe:
+                        policyBuilder.Append(_unsafeInlineSource);
+                        break;
+                    case ContentSecurityPolicyInlineExecution.Nonce:
+                        string nonceRandom = Guid.NewGuid().ToString("N");
+                        filterContext.HttpContext.Items[NonceRandomContextKey] = nonceRandom;
+                        policyBuilder.AppendFormat(_nonceSourceFormat, nonceRandom);
+                        break;
+                    case ContentSecurityPolicyInlineExecution.Hash:
+                        filterContext.HttpContext.Items[ScriptHashListBuilderContextKey] = new StringBuilder();
+                        policyBuilder.Append(_scriptHashListPlaceholder);
+                        break;
+                    default:
+                        break;
+                }
+
+                policyBuilder.Append(_directivesDelimiter);
+            }
+
+            if (policyBuilder.Length > 0)
+            {
+                filterContext.HttpContext.Response.AppendHeader(_contentSecurityPolicyHeader, policyBuilder.ToString());
+            }
+        }
+        #endregion
+
+        #region IResultFilter Members
+        /// <summary>
+        /// Called after an action result executes.
+        /// </summary>
+        /// <param name="filterContext">The filter context.</param>
+        public void OnResultExecuted(ResultExecutedContext filterContext)
+        {
+            if (ScriptInlineExecution == ContentSecurityPolicyInlineExecution.Hash)
+            {
+                filterContext.HttpContext.Response.Headers[_contentSecurityPolicyHeader] = filterContext.HttpContext.Response.Headers[_contentSecurityPolicyHeader].Replace(_scriptHashListPlaceholder, ((StringBuilder)filterContext.HttpContext.Items[ScriptHashListBuilderContextKey]).ToString());
+            }
+        }
+
+        /// <summary>
+        /// Called before an action result executes.
+        /// </summary>
+        /// <param name="filterContext">The filter context.</param>
+        public void OnResultExecuting(ResultExecutingContext filterContext)
+        { }
+        #endregion
+    }
+}
---
+++ Lib.Web.Mvc/Html/ContentSecurityPolicyExtensions.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web.Mvc;
+
+namespace Lib.Web.Mvc.Html
+{
+    /// <summary>
+    /// Provides support for Content Security Policy Level 2 protected elements.
+    /// </summary>
+    public static class ContentSecurityPolicyExtensions
+    {
+        #region Methods
+        /// <summary>
+        /// Writes an opening script tag to the response, and sets attributes related to Content Security Policy
+        /// </summary>
+        /// <param name="htmlHelper">The HTML helper</param>
+        /// <returns></returns>
+        public static IDisposable BeginCspScript(this HtmlHelper htmlHelper)
+        {
+            return BeginCspScript(htmlHelper, null);
+        }
+
+        /// <summary>
+        /// Writes an opening script tag to the response, and sets attributes related to Content Security Policy
+        /// </summary>
+        /// <param name="htmlHelper">The HTML helper</param>
+        /// <param name="htmlAttributes">An object that contains the HTML attributes to set for the element</param>
+        /// <returns></returns>
+        public static IDisposable BeginCspScript(this HtmlHelper htmlHelper, object htmlAttributes)
+        {
+            return new ContentSecurityPolicyScript(htmlHelper.ViewContext, HtmlHelper.AnonymousObjectToHtmlAttributes(htmlAttributes));
+        }
+
+        /// <summary>
+        /// Writes an opening script tag to the response, and sets attributes related to Content Security Policy
+        /// </summary>
+        /// <param name="htmlHelper">The HTML helper</param>
+        /// <param name="htmlAttributes">An object that contains the HTML attributes to set for the element</param>
+        /// <returns></returns>
+        public static IDisposable BeginCspScript(this HtmlHelper htmlHelper, IDictionary<string, object> htmlAttributes)
+        {
+            return new ContentSecurityPolicyScript(htmlHelper.ViewContext, htmlAttributes);
+        }
+        #endregion
+
+        #region Classes
+        private class ContentSecurityPolicyScript : IDisposable
+        {
+            #region Fields
+            private readonly ViewContext _viewContext;
+            private readonly ContentSecurityPolicyInlineExecution _scriptInlineExecution;
+            private readonly int _viewBuilderIndex;
+            private readonly TagBuilder _scriptTag;
+            #endregion
+
+            #region Constants
+            private const string _scriptTagName = "script";
+            private const string _nonceAttribute = "nonce";
+            private const string _sha256SourceFormat = " 'sha256-{0}'";
+            #endregion
+
+            #region Constructor
+            public ContentSecurityPolicyScript(ViewContext context, IDictionary<string, object> htmlAttributes)
+            {
+                _viewContext = context;
+
+                _scriptInlineExecution = (ContentSecurityPolicyInlineExecution)_viewContext.HttpContext.Items[ContentSecurityPolicyAttribute.ScriptInlineExecutionContextKey];
+
+                _scriptTag = new TagBuilder(_scriptTagName);
+                _scriptTag.MergeAttributes(htmlAttributes);
+                if (_scriptInlineExecution == ContentSecurityPolicyInlineExecution.Nonce)
+                {
+                    _scriptTag.MergeAttribute(_nonceAttribute, (string)_viewContext.HttpContext.Items[ContentSecurityPolicyAttribute.NonceRandomContextKey]);
+                }
+
+                _viewContext.Writer.Write(_scriptTag.ToString(TagRenderMode.StartTag));
+
+                if (_scriptInlineExecution == ContentSecurityPolicyInlineExecution.Hash)
+                {
+                    _viewBuilderIndex = ((StringWriter)_viewContext.Writer).GetStringBuilder().Length;
+                }
+            }
+            #endregion
+
+            #region IDisposable Members
+            public void Dispose()
+            {
+                if (_scriptInlineExecution == ContentSecurityPolicyInlineExecution.Hash)
+                {
+                    StringBuilder viewBuilder = ((StringWriter)_viewContext.Writer).GetStringBuilder();
+                    string scriptContent = viewBuilder.ToString(_viewBuilderIndex, viewBuilder.Length - _viewBuilderIndex).Replace("\r\n", "\n");
+                    byte[] scriptHashBytes = new SHA256Managed().ComputeHash(Encoding.UTF8.GetBytes(scriptContent));
+                    string scriptHash = Convert.ToBase64String(scriptHashBytes);
+                    ((StringBuilder)_viewContext.HttpContext.Items[ContentSecurityPolicyAttribute.ScriptHashListBuilderContextKey]).AppendFormat(_sha256SourceFormat, scriptHash);
+                }
+                _viewContext.Writer.Write(_scriptTag.ToString(TagRenderMode.EndTag));
+            }
+            #endregion
+        }
+        #endregion
+    }
+}
