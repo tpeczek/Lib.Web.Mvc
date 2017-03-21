e18a903bf8a466c861c3c28694f1e4f322444607
Tomasz Pęczek <tpeczek@gmail.com>
2017-03-21 22:05:55 +0100

    FIPS compliant ETag generation. Resolves #101.

---
+++ Lib.Web.Mvc/Http/EntityTagGenerator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace Lib.Web.Mvc.Http
+{
+    internal static class EntityTagGenerator
+    {
+        internal static string GenerateModificationDateAndLengthXOREntityTag(DateTime modificationDate, long length)
+        {
+            long enityTagHash = modificationDate.ToFileTime() ^ length;
+            return Convert.ToString(enityTagHash, 16);
+        }
+
+        [Obsolete("This method is not FIPS compliant.")]
+        internal static string GenerateNameAndModificationDateMD5EntityTag(string name, DateTime modificationDate)
+        {
+            byte[] entityTagBytes = Encoding.ASCII.GetBytes(String.Format("{0}|{1}", name, modificationDate));
+            return Convert.ToBase64String(new MD5CryptoServiceProvider().ComputeHash(entityTagBytes));
+        }
+    }
+}
--- Lib.Web.Mvc/Properties/AssemblyInfo.cs
+++ Lib.Web.Mvc/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Tomasz Pęczek")]
 [assembly: AssemblyProduct("Lib.Web.Mvc")]
-[assembly: AssemblyCopyright("Copyright © 2009 - 2016 Tomasz Pęczek")]
+[assembly: AssemblyCopyright("Copyright © 2009 - 2017 Tomasz Pęczek")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
